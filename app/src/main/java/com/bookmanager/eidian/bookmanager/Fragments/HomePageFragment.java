733558ae5006ed11740ad76a07521eeef7b121c4
lune <915859546@qq.com>
2016-09-29 22:08:06 +0800

    主页

---
+++ app/src/main/java/com/bookmanager/eidian/bookmanager/Fragments/HomePageFragment.java
@@ -0,0 +1,29 @@
+package com.xiang.text4.Fragments;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.xiang.text4.R;
+
+// 顾名思义，主页的Fragment
+
+public class HomePageFragment extends Fragment {
+
+
+    public HomePageFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_page, container, false);
+    }
+
+}
