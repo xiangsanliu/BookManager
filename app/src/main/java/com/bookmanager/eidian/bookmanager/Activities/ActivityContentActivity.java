3c41fe92693d04e699cd8860fe2ffc0c5b6374c4
xiang_36 <xiangjianjian@hotmail.com>
2016-10-08 13:19:54 +0800

    1. 第四次提交 2. 添加了新闻显示界面的时间

--- app/src/main/java/com/bookmanager/eidian/bookmanager/Activities/ActivityContentActivity.java
+++ app/src/main/java/com/bookmanager/eidian/bookmanager/Activities/ActivityContentActivity.java
@@ -7,10 +7,12 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bookmanager.eidian.bookmanager.Helpers.Connection;
 import com.bookmanager.eidian.bookmanager.R;
 import com.squareup.picasso.Picasso;
 
@@ -45,6 +47,9 @@ public class ActivityContentActivity extends AppCompatActivity {
                     String photoUrl = (String) msg.obj;
                     Picasso.with(imageView.getContext()).load(photoUrl).into(imageView);
                     break;
+                case 2:
+                    String date_str = (String) msg.obj;
+                    ((TextView) findViewById(R.id.news_date)).setText(date_str);
             }
         }
     };
@@ -88,6 +93,11 @@ public class ActivityContentActivity extends AppCompatActivity {
 
                     Document document = Jsoup.parse(datas);
                     Elements elements = document.select("div.table-responsive");
+                    Elements date = document.select("h5.hn_news_subtitle");
+                    String str = date.text();
+                    str = str.substring(0,17);
+                    Log.d("date", str);
+
                     Elements element = elements.get(0).getElementsByTag("p");
                     String content = "";
                     for (int i = 0; i < element.size(); i++) {
@@ -109,6 +119,11 @@ public class ActivityContentActivity extends AppCompatActivity {
                     message1.what = 1;
                     message1.obj = photoUrl;
                     handler.sendMessage(message1);
+
+                    Message message2 = new Message();
+                    message2.what = 2;
+                    message2.obj = str;
+                    handler.sendMessage(message2);
 
                 } catch (Exception e) {
                     e.printStackTrace();
